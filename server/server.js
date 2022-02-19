983fa40fabf840a73c00bf9d5dbcd57d673def77
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2022-02-18 20:07:13 -0600

    enabled cors npm to get rid of cors policy errors

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
+
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+
 app.use(express.json());
-app.use(require("./routes/note-routes"));
-// get driver connection
+
+app.use(require("./routes/note-routes", cors()));
+// import the database connection
 const dbo = require("./db/connection");
 
 app.listen(port, () => {
